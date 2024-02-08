c9cc49735a9b40570d8b3778dd25ee44413779cf
leeroy Phili <leeroy@myshoperon.com>
2024-02-08 16:27:49 -0500

    minor changes

---
+++ my-next-app-on-amplify/src/app/ssr.js
@@ -0,0 +1,14 @@
+export default function SSR() {
+    return (
+      <>
+        <h1>Server-side rendered page</h1>
+        <p>
+          This page is server-side rendered. It was rendered on 02/08/2024.
+        </p>
+        <p>
+          <a href="/">View a static page.</a>
+        </p>
+      </>
+    );
+  }
+  
